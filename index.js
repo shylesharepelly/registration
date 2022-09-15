d5cf7683e0c7c73b903dbdfc880fd94485879bf8
shylesharepelly <66762947+shylesharepelly@users.noreply.github.com>
2022-09-15 19:43:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const displayEntries = () => {
       <th class="px-4 py-2">Email</th>
       <th class="px-4 py-2">Password</th>
       <th class="px-4 py-2">Dob</th>
-      <th class="px-4 py-2">Accepted terms?</th>
+      <th class="px-4 py-2">Accepted terms</th>
    </tr>${tableEntries}</table>`;
 
   let details = document.getElementById("user-entries");
